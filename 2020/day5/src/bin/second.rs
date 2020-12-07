6b551a45ac968d775e0c2d2e25dc7f4cea27ddd9
Rogier 'DocWilco' Mulhuijzen <github@bsdchicks.com>
2020-12-07 18:08:23 +0100

    Why multiply by 8 is same as shiftleft 3

--- 2020/day5/src/bin/second.rs
+++ 2020/day5/src/bin/second.rs
@@ -1,39 +1,34 @@
 use anyhow::{anyhow, Result};
 use num::{One, Zero};
-use std::{fs, mem::size_of, ops::{BitOrAssign, ShlAssign}};
+use std::{
+    fs,
+    mem::size_of,
+    ops::{BitOrAssign, ShlAssign},
+};
 
 fn bsp_to_number<T: One + Zero + ShlAssign<u8> + BitOrAssign>(input: &str) -> Result<T> {
     if input.len() / 8 > size_of::<T>() {
-        return Err(anyhow!("Input bits ({}) exceed available in {}", input.len(), std::any::type_name::<T>()));
+        return Err(anyhow!(
+            "Input bits ({}) exceed available in {}",
+            input.len(),
+            std::any::type_name::<T>()
+        ));
     }
     let mut result: T = <T>::zero();
     for bit in input.chars() {
         result <<= 1;
         if bit == 'B' || bit == 'R' {
             result |= <T>::one();
-        } 
+        }
     }
     Ok(result)
 }
 
-fn bsp_seat_to_id(bsp_seat: &str) -> Result<u32> {
-    if bsp_seat.len() != 10 {
-        Err(anyhow!("BSP seat ID not 10 characters"))
-    } else {
-        let (row, column) = bsp_seat.split_at(7);
-        let column: u32 = bsp_to_number(column)?;
-        let row: u32 = bsp_to_number(row)?;
-        let id = (row * 8) + column;
-        Ok(id)
-    }
-}
-
-fn highest_seatid(input: &str) -> u32 {
-    input.lines().map(|seat| bsp_seat_to_id(seat).unwrap()).max().unwrap()
-}
-
 fn find_missing(input: &str) -> Result<u32> {
-    let seats = input.lines().map(|seat| bsp_seat_to_id(seat).unwrap()).collect::<Vec<_>>();
+    let seats = input
+        .lines()
+        .map(|seat| bsp_to_number(seat).unwrap())
+        .collect::<Vec<u32>>();
     let min = seats.iter().min().unwrap().clone();
     let max = seats.iter().max().unwrap().clone();
     for i in min..=max {
@@ -67,14 +62,6 @@ mod tests {
     #[test_case("FFFBBBFRRR" => 119)]
     #[test_case("BBFFBBFRLL" => 820)]
     fn id(input: &str) -> u32 {
-        bsp_seat_to_id(input).unwrap()
-    }
-
-    #[test]
-    fn highest() {
-        assert_eq!(highest_seatid("FBFBBFFRLR
-BFFFBBFRRR
-FFFBBBFRRR
-BBFFBBFRLL"), 820);
+        bsp_to_number(input).unwrap()
     }
 }
