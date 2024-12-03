3624a5e5121347ab2218ae18e3e5cd22e0e4b1aa
DocWilco <github@drwil.co>
2024-12-03 12:42:13 +0100

    rename tests

--- 2024/day3/src/bin/first.rs
+++ 2024/day3/src/bin/first.rs
@@ -34,7 +34,7 @@ mod tests {
     use super::*;
     use test_case::test_case;
 
-    #[test_case("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))" => 161)]
+    #[test_case("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))" => 161; "example")]
     fn test(input: &str) -> u64 {
         run(input)
     }
--- 2024/day3/src/bin/second.rs
+++ 2024/day3/src/bin/second.rs
@@ -48,8 +48,8 @@ mod tests {
     use super::*;
     use test_case::test_case;
 
-    #[test_case("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))" => 161)]
-    #[test_case("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))" => 48)]
+    #[test_case("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))" => 161; "example from first part")]
+    #[test_case("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))" => 48; "example from second part")]
     fn test(input: &str) -> u64 {
         run(input)
     }
