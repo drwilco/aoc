d1fed96a54c8afb1faebd09b26e2fbe90c4982fc
DocWilco <github@drwil.co>
2024-12-04 15:10:31 +0100

    Add benching to template

--- template/src/bin/first.rs
+++ template/src/bin/first.rs
@@ -1,3 +1,5 @@
+#![feature(test)]
+
 use std::fs;
 
 fn run(input: &str) -> i64 {
@@ -11,8 +13,17 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    extern crate test as std_test;
     use super::*;
+    use std_test::{black_box, Bencher};
     use test_case::test_case;
+
+    #[bench]
+    fn my_benchmark(b: &mut Bencher) {
+        let input = fs::read_to_string("input.txt").unwrap();
+        let input = black_box(&input);
+        b.iter(|| run(input));
+    }
 
     #[test_case("Hello world!" => 0)]
     fn test(input: &str) -> i64 {
