ea26433c2ea0adc943d5a63b70348510f918d720
DocWilco <github@drwil.co>
2024-12-04 14:58:28 +0100

    Use ndarray windows()

--- 2024/day4/src/bin/second.rs
+++ 2024/day4/src/bin/second.rs
@@ -1,15 +1,9 @@
 #![feature(test)]
-use itertools::Itertools;
+
 use ndarray::Array;
 use std::fs;
 
 fn run(input: &str) -> usize {
-    // Only looking for diagonals now
-    let directions = [-1, 1]
-        .into_iter()
-        .cartesian_product([-1, 1])
-        .collect::<Vec<_>>();
-    let pattern = ['M', 'A', 'S'];
     let (elements, (Some(width), height)) = input.lines().fold(
         (Vec::with_capacity(input.len()), (None, 0)),
         |(mut chars, (mut width, height)), line| {
@@ -23,50 +17,25 @@ fn run(input: &str) -> usize {
         panic!("Empty input")
     };
     let grid = Array::from_shape_vec((height, width), elements).unwrap();
-    grid.indexed_iter().fold(0, |acc, ((y, x), &c)| {
-        // Let's scan for `A` because it's the center
-        if c != pattern[1] {
-            return acc;
-        }
-        let matches = directions
-            .iter()
-            // First find all the directions that we have a match in
-            .filter(|&direction| {
-                let mut matched = 0_usize;
-                for (i, &p) in pattern.iter().enumerate() {
-                    // We are centering on `A`, so start at -1, so we check -1,
-                    // 0, 1
-                    let i = isize::try_from(i).unwrap() - 1;
-                    let (Some(y), Some(x)) = (
-                        y.checked_add_signed(direction.0 * i),
-                        x.checked_add_signed(direction.1 * i),
-                    ) else {
-                        break;
-                    };
-                    let Some(&to_check) = grid.get((y, x)) else {
-                        break;
-                    };
-                    if to_check == p {
-                        matched += 1;
-                    } else {
-                        break;
-                    }
-                }
-                if matched == 3 {
-                    true
-                } else {
-                    false
-                }
-            })
-            // Since we're only checking diagonals, if we have 2 matches,
-            // they're guaranteed to be perpendicular
-            .count();
-        if matches == 2 {
-            acc + 1
-        } else {
-            acc
-        }
-    })
+    grid.windows((3, 3))
+        .into_iter()
+        .filter(|window| {
+            let center = window[[1, 1]];
+            if center != 'A' {
+                return false;
+            }
+            let corners = [
+                window[[0, 0]],
+                window[[0, 2]],
+                window[[2, 0]],
+                window[[2, 2]],
+            ];
+            corners == ['M', 'M', 'S', 'S']
+                || corners == ['S', 'M', 'M', 'S']
+                || corners == ['S', 'S', 'M', 'M']
+                || corners == ['M', 'S', 'S', 'M']
+        })
+        .count()
 }
 
 fn main() {
@@ -78,7 +47,7 @@ fn main() {
 mod tests {
     extern crate test as std_test;
     use super::*;
-    use std_test::{Bencher, black_box};
+    use std_test::{black_box, Bencher};
     use test_case::test_case;
 
     #[bench]
